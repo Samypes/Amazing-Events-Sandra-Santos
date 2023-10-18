9a23a8381dc45e8312d071061e6d776c2e94aa3b
samype <samype218@gmail.com>
2023-10-18 10:34:23 -0500

    arreglos para sprint2

---
+++ UpComingEvents.js
@@ -0,0 +1,18 @@
+const arrayEvents = data.events
+const contenedor = document.getElementById("contenedorCards")
+
+for (let events  of arrayEvents) {
+  const card = document.createElement("div")
+card.classList.add("card")
+card.style.width = "18rem"
+card.innerHTML =`<img src="${events.image}" class="card-img-top" alt="cards">
+<div class="card-body " >
+  <h5 class="card-title">${events.name}</h5>
+  <p class="cartext" >"${events.description}."
+  </p>
+  <p> ${events.price}</p>
+  <a href="Details.html" class="btn btn-primary">Details</a>
+</div>`
+contenedor.appendChild(card)
+
+}
