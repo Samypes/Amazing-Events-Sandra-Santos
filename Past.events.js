96ec9796b0807309c0b17f3f29a6c908e6bd31df
samype <samype218@gmail.com>
2023-10-19 20:09:24 -0500

    ajustes finales sprint 2

--- Past.events.js
+++ Past.events.js
@@ -1,11 +1,22 @@
-const arrayEvents = data.events
+const arrayEvents = data.events;
+const arrayDate = []
+for (let events of arrayEvents) {
+  if (events.date < "2023-01-01") {
+    arrayDate.push(events)
+
+    console.log();
+  }
+}
+
+console.log(arrayDate);
+
 const contenedor = document.getElementById("contenedorCards")
 
-for (let events  of arrayEvents) {
+for (let events of arrayDate) {
   const card = document.createElement("div")
-card.classList.add("card")
-card.style.width = "18rem"
-card.innerHTML =`<img src="${events.image}" class="card-img-top" alt="cards">
+  card.classList.add("card")
+  card.style.width = "18rem"
+  card.innerHTML = `<img src="${events.image}" class="card-img-top" alt="cards">
 <div class="card-body " >
   <h5 class="card-title">${events.name}</h5>
   <p class="cartext" >"${events.description}."
@@ -13,6 +24,6 @@ card.innerHTML =`<img src="${events.image}" class="card-img-top" alt="cards">
   <p> ${events.price}</p>
   <a href="Details.html" class="btn btn-primary">Details</a>
 </div>`
-contenedor.appendChild(card)
+  contenedor.appendChild(card)
 
 }
